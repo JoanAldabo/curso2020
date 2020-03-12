8f33adcbfc2a1bdeb1a9eb79ba2e39b97f09408f
Joan <joan.aldabo.perez@gmail.com>
2020-03-12 18:20:37 +0100

    Temporal, thursday

--- helpdesk-curso/wizard/helpdesk_set_responsable.py
+++ helpdesk-curso/wizard/helpdesk_set_responsable.py
@@ -4,8 +4,20 @@ from odoo import fields, models, api, _
 class HelpDeskSetResponsable(models.TransientModel):
     _name = 'helpdesk.set.responsable.wiz'
 
+    tickets_qty = fields.Integer(string="Tickets Qty")
+
+    @api.model
+    def _default_get(self, fields):
+        res = super(HelpDeskSetResponsable, self).default_get(fields)
+        qty = len(self.env["helpdesk.ticket"].search([('responsable_id', '=', self.env.uid)]))
+        res['tickets_qty'] = qty
+        return res
+
     def set_current_user_as_responsable(self):
         self.ensure_one()
+        active_id = self.env.context.get('active_id')
+        active_model = self.env.context.get('active_model')
 
-        ticket = self.env["helpdesk.ticket"].search([("id", "=", self.env.context.get('active_id'))])
-        ticket.responsable_id = self.env.user
+        if active_id and active_model and active_model == "helpdesk.ticket":
+            ticket = self.env["helpdesk.ticket"].browse(active_id)
+            ticket.responsable_id = self.env.user
